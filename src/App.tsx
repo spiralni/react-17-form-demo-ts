16c4cce0fd74d112996977c7a75ad214f4b7c555
Cesar Lopez <clopez@logicasoft.io>
2021-07-23 13:27:26 -0600

    Removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import { useState } from 'react'
 import './App.css'
 import ControlledForm from './components/ControlledForm';
-import Focus from './components/Focus';
-import UncontrolledForm from './components/UncontrolledForm'
 
 function App() {
   return <div className="App">
